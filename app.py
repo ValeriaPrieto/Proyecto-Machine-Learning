c42f27b0dd5412dcb3aed27f0faad5c21b1127ff
ValeriaPrieto <102117794+ValeriaPrieto@users.noreply.github.com>
2025-05-25 21:56:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,9 @@ def load_dataset():
                 IQR = Q3 - Q1
                 lower_bound = Q1 - 1.5 * IQR
                 upper_bound = Q3 + 1.5 * IQR
-                df = df[(df[col] >= lower_bound) & (df[col] <= upper_bound)]
+                mask &= df[col].between(lower_bound, upper_bound)
+
+    df = df[mask]
     df = df.drop(columns=['CustomerNo', 'ProductNo','TransactionNo'])
     df = df.sample(n=10000, random_state=42)
     df.reset_index(drop=True, inplace=True)
